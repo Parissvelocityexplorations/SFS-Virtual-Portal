d733b9471e34b2f05e7027f4bcd16bb94b3bada4
Jack <jack@Jacks-MacBook-Pro.local>
2025-04-23 12:11:21 -0400

    jackbranch kiosk page

---
+++ client/app/routes/KioskHome.tsx
@@ -0,0 +1,17 @@
+// KioskHome.tsx
+import React from 'react';
+import '../styles/theme.css';
+
+
+export default function KioskHome() {
+  return (
+    <html lang="en">
+      <head>
+        <title>Kiosk Home</title>
+      </head>
+      <body>
+        <h1>Welcome to the Kiosk</h1>
+      </body>
+    </html>
+  );
+}
